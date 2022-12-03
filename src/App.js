52a89727af04b129878dc343b2ef3e9c206aa432
vikhil0609 <sallagargivikhil123@gmail.com>
2022-12-03 20:53:25 +0530

    progress in navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import NavbarPage from "./pages/navbarpage.js";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <NavbarPage />
         <Routes>
-          <Route exact path="/" element={<h1>HOME</h1>}></Route>
+          <Route exact path="/" element={<h1> </h1>}></Route>
+          <Route exact path="/home" element={<h1> </h1>}></Route>
           <Route exact path="/about" element={<h1>ABOUT</h1>}></Route>
           <Route exact path="/contact" element={<h1>CONTACT</h1>}></Route>
         </Routes>
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,8 +1,29 @@
 import React from "react";
+import "../../assets/css/navbar.css";
 
 class Navbar extends React.Component {
   render() {
-    return <div className="navbar-main">HELLO WORLD</div>;
+    return (
+      <nav className="navbar-main">
+        <div>
+          <h1>THIS IS LOGO</h1>
+        </div>
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/courses">Courses</a>
+          </li>
+          <li>
+            <a href="/about">About Us</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    );
   }
 }
 
