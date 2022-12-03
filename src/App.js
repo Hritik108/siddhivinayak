0013c226e9227a98dd6ed8507c0200f83ff0168d
Hritik chauhan <chauhanritik786@gmail.com>
2022-12-03 19:43:50 +0530

    createaboutuspg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import AboutUs from "./components/aboutUs";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route exact path="/" element={<h1>HOME</h1>}></Route>
-          <Route exact path="/about" element={<h1>ABOUT</h1>}></Route>
+          <Route exact path="/about" element={<AboutUs/>}></Route>
           <Route exact path="/contact" element={<h1>CONTACT</h1>}></Route>
         </Routes>
       </div>
