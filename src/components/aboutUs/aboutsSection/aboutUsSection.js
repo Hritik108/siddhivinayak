013135ef00f0362f07c7fca24a24f14a86af8e40
vikhil0609 <sallagargivikhil123@gmail.com>
2022-12-05 18:27:17 +0530

    about us review

---
+++ src/components/aboutUs/aboutsSection/aboutUsSection.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./aboutUsSection.css";
+import "./aboutUsVariousSection.css";
+
+class AboutUsSection extends Component {
+  render() {
+    return (
+      <div className={this.props.AboutUsSectionCss + " AboutUsSection"}>
+        <h2>{this.props.contenthead}</h2>
+        <p className="AboutUsSectionp">
+          AboutUsSection {this.props.contentbody}
+        </p>
+      </div>
+    );
+  }
+}
+
+export default AboutUsSection;
