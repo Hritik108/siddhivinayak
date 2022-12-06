9d88712d907894d66035453cda5c44fa2edca7eb
Prashant Vasala <pvasala786@gmail.com>
2022-12-06 10:53:56 +0530

    Little updates on contact page

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "../navbar/navbar.js";
 import "../../assets/css/contact.css";
-import { FaPhoneAlt, FaRegEnvelope } from "react-icons/fa";
-import { IoLocationSharp } from "react-icons/io5";
+import { IoCall, IoLocationSharp } from "react-icons/io5";
 
 class Contact extends Component {
   render() {
@@ -11,19 +10,23 @@ class Contact extends Component {
         <Navbar />
         <div className="contact">
           <div className="contact-body">
-            <h1>Get in touch</h1>
-            <p>
-              Want to get in touch? We'd love to hear from you. Here's how you
-              can reach us...
-            </p>
+            <div className="contact-headings">
+              <h1>Get in touch</h1>
+              <p>
+                Want to get in touch? We'd love to hear from you. Here's how you
+                can reach us...
+              </p>
+            </div>
+
             <div className="cards">
               <div className="card">
                 <div className="card-icon">
-                  <FaPhoneAlt />
+                  <IoCall />
                 </div>
-                <h2 className="card-title">Talk to Us</h2>
+                <h1 className="card-title">Talk to Us</h1>
                 <p className="card-text">
                   Interested to join? You can feel free to call us anytime
+                  <br />
                   during our working hours on the following contact numbers
                 </p>
                 <ul>
@@ -39,10 +42,17 @@ class Contact extends Component {
                 <div className="card-icon">
                   <IoLocationSharp />
                 </div>
-                <h2 className="card-title">Find Us Here</h2>
+                <h1 className="card-title">Find Us Here</h1>
                 <p className="card-text">
-                  Shop No 6, Siddhivinayak Classes, Agar Bazaar, Dadar West,
-                  Mumbai - 400028, Near Agar Bazar Bus Stop
+                  Shop No 6, Siddhivinayak Classes,
+                  <br /> Near Agar Bazar Bus Stop, Agar Bazaar, Dadar West,
+                  <br /> Mumbai - 400028
+                </p>
+                <p className="card-text">
+                  At Ratan, Shop No: 10, S.K. Bole Road,
+                  <br /> Agar Bazaar, Near Siddhivinayak Temple, Dadar West,
+                  <br />
+                  Mumbai - 400028
                 </p>
               </div>
             </div>
