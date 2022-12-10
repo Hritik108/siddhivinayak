7d9cd0f242b87efe19fa89e74246ed039065f43b
Prashant Vasala <pvasala786@gmail.com>
2022-12-10 13:21:47 +0530

    Make changes in this footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
 import "../../assets/css/footer.css";
 
-class Footer extends Component {
+class Footer extends React.Component {
   render() {
     return (
-      <footer>
-        <div className="footerContainer">
-          <h1>This is Footer</h1>
+      <div className="footerContainer">
+        <div className="footerContent">
+          <div className="footerLogo">
+            <h1>This is logo</h1>
+            <p>
+              Darpan is a youth club empowered by its members with enthusuiasm
+              to contribute towards the academic, personal, social & cultural
+              development of individuals by inclucating the right education &
+              guidance.
+            </p>
+          </div>
         </div>
-      </footer>
+      </div>
     );
   }
 }
+
 export default Footer;
