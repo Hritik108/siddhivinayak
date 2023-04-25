1c469d0ce6eee75344c0ad794382e8c764aca664
Prashant33-py <pvasala786@gmail.com>
2023-04-25 21:52:05 +0530

    Changed the course page layout

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,167 +1,90 @@
 import * as React from "react";
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import education from "./Courses.images/education.jpg";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import "./courses.css";
 // import { Link} from 'react-scroll'
 
 const cards = [
-    { id: "#h1", course: "SSC", description: "Shree Siddhivinayak Classes provides comprehensive coaching and guidance to Maharashtra SSC board students with expert faculty, well-designed study material, and regular tests and assessments to ensure academic success." },
-    { id: "#h2", course: "HSC", description: "Shree Siddhivinayak Classes provides comprehensive coaching and guidance to Maharashtra HSC Board students, helping them to excel in their studies and achieve their academic goals. Their programs cover all the key subjects, including Mathematics, Science, and Commerce, and are designed to build a strong foundation of knowledge and skills for future success." },
-    { id: "#h3", course: "JEE", description: "Shree Siddhivinayak Classes provides comprehensive coaching for Maharashtra JEE Main aspirants, covering all the important topics and concepts necessary to excel in the exam. They offer expert guidance, practice materials, and regular assessments to help students achieve their academic goals." },
+    {
+        id: "#h1",
+        course: "SSC",
+        description:
+            "Shree Siddhivinayak Classes provides comprehensive coaching and guidance to Maharashtra SSC board students with expert faculty, well-designed study material, and regular tests and assessments to ensure academic success.",
+    },
+    {
+        id: "#h2",
+        course: "HSC",
+        description:
+            "Shree Siddhivinayak Classes provides comprehensive coaching and guidance to Maharashtra HSC Board students, helping them to excel in their studies and achieve their academic goals. Their programs cover all the key subjects, including Mathematics, Science, and Commerce, and are designed to build a strong foundation of knowledge and skills for future success.",
+    },
+    {
+        id: "#h3",
+        course: "JEE",
+        description:
+            "Shree Siddhivinayak Classes provides comprehensive coaching for Maharashtra JEE Main aspirants, covering all the important topics and concepts necessary to excel in the exam. They offer expert guidance, practice materials, and regular assessments to help students achieve their academic goals.",
+    },
 ];
 const theme = createTheme();
 let n = 1;
 
 export default function Courses() {
+    let courseDetails = {
+        title:
+            "Special Features of Shree Siddhivinayak Classes for SSC Students",
+        features: [
+            {
+                featureTitle: "Experienced Faculty",
+                featureDescription:
+                    "The institute has a team of highly experienced and dedicated faculty members who provide personalized attention to each student and help them improve their academic performance.",
+            },
+            {
+                featureTitle: "Comprehensive Study Material",
+                featureDescription:
+                    "Shree Siddhivinayak Classes provides comprehensive study material that covers the entire SSC syllabus, including solved examples and practice questions.",
+            },
+            {
+                featureTitle: "Regular Assessments",
+                featureDescription:
+                    "The institute conducts regular assessments, including mock tests and practice papers, to help students evaluate their progress and identify areas for improvement.",
+            },
+            {
+                featureTitle: "Doubt-solving Sessions",
+                featureDescription:
+                    "Shree Siddhivinayak Classes conducts regular doubt-solving sessions, where students can clarify their doubts and get individual attention from the faculty.",
+            },
+        ],
+    };
     return (
-        <ThemeProvider theme={theme}>
-            {/* <CssBaseline /> */}
-            <main>
-                {/* Hero unit */}
-                <Box
-                    sx={{
-                        bgcolor: "background.paper",
-                        pt: 8,
-                        pb: 6,
-                    }}
-                >
-                    <Container maxWidth="sm">
-                        <Typography component="h1" variant="h2" align="center" color="text.primary" gutterBottom>
-                            Student Courses
-                        </Typography>
-                        <Typography variant="h5" align="center" color="text.secondary" paragraph>
-                            Check out the courses that we offer to the students in different branches and fields of study
-                        </Typography>
-                    </Container>
-                </Box>
-                <Container sx={{ py: 8 }} maxWidth="lg">
-                    {/* End hero unit */}
-                    <Grid container spacing={4}>
-                        {cards.map((card) => (
-                            <Grid item key={card} xs={12} sm={6} md={4}>
-                                <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
-                                    <CardMedia
-                                        component="img"
-                                        sx={{
-                                            // 16:9
-                                            pt: "2%",
-                                        }}
-                                        image={education}
-                                        alt="random"
-                                    />
-                                    <CardContent sx={{ flexGrow: 1 }}>
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                            {card.course}
-                                        </Typography>
-                                        <Typography>{card.description}</Typography>
-                                    </CardContent>
-                                    <CardActions>
-                                        {/* <Button size="small">View</Button> */}
-                                        <Button>
-                                            <a href={card.id}> Read More</a>
-                                        </Button>
-                                    </CardActions>
-                                </Card>
-                            </Grid>
-                        ))}
-                    </Grid>
-                </Container>
-
-                <Container sx={{ py: 8 }} maxWidth="lg">
-                    <Grid container spacing={4}>
-                        <Grid item xs={12} sm={12} md={12} id="h1">
-                            <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
-                                <CardMedia
-                                    component="img"
-                                    sx={{
-                                        // 16:9
-                                        pt: "2%",
-                                    }}
-                                    image={education}
-                                    alt="random"
-                                />
-                                <CardContent sx={{ flexGrow: 1 }}>
-                                    <Typography gutterBottom variant="h5" component="h2">
-                                        {cards[0].course}
-                                    </Typography>
-                                    <Typography>{cards[0].description}</Typography>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-
-                        <Grid item xs={12} sm={12} md={12} id="h2">
-                            <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
-                                <CardMedia
-                                    component="img"
-                                    sx={{
-                                        // 16:9
-                                        pt: "2%",
-                                    }}
-                                    image={education}
-                                    alt="random"
-                                />
-                                <CardContent sx={{ flexGrow: 1 }}>
-                                    <Typography gutterBottom variant="h5" component="h2">
-                                        {cards[1].course}
-                                    </Typography>
-                                    <Typography>{cards[1].description}</Typography>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-
-                        <Grid item xs={12} sm={12} md={12} id="h3">
-                            <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
-                                <CardMedia
-                                    component="img"
-                                    sx={{
-                                        // 16:9
-                                        pt: "2%",
-                                    }}
-                                    image={education}
-                                    alt="random"
-                                />
-                                <CardContent sx={{ flexGrow: 1 }}>
-                                    <Typography gutterBottom variant="h5" component="h2">
-                                        {cards[2].course}
-                                    </Typography>
-                                    <Typography>{cards[2].description}</Typography>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                    </Grid>
-                    {/* <Container md={6} sm={6} xs={12} sx={{
-    bgcolor: 'blue'
-  }}>hello</Container>
-  <Container md={6} sm={12} xs={12} sx={{
-    bgcolor: 'pink'
-  }}>nfodofo</Container> */}
-                </Container>
-            </main>
-            {/* Footer */}
-            {/* <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="text.secondary"
-          component="p"
-        >
-          Something here to give the footer a purpose!
-        </Typography>
-        <Copyright />
-      </Box> */}
-            {/* End footer */}
-        </ThemeProvider>
+        <div className="container mt-5 text-center">
+            <img
+                src="https://ischoolconnect.com/blog/wp-content/uploads/2021/10/564.jpg"
+                alt=""
+            />
+            <h1 className="text-start">SSC</h1>
+            <p className="text-start">
+                Shree Siddhivinayak Classes provides comprehensive coaching and
+                guidance to Maharashtra SSC board students with expert faculty,
+                well-designed study material, and regular tests and assessments
+                to ensure academic success.
+            </p>
+            <div className="d-flex row mx-auto w-75 text-center mb-3">
+                <h2 className="my-3">{courseDetails.title}</h2>
+                {courseDetails.features.map((feature) => {
+                    return (
+                        <div className="col-sm-3">
+                            <div className="card w-100 h-100">
+                                <div className="card-body">
+                                    <h5 className="card-title">
+                                        {feature.featureTitle}
+                                    </h5>
+                                    <p className="card-text">
+                                        {feature.featureDescription}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
     );
 }
