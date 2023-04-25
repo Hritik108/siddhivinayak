86bb6dc0423701cec1bf4218a4a325afbe31ff93
Prashant33-py <pvasala786@gmail.com>
2023-04-25 20:21:13 +0530

    Little changes in navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,146 +1,151 @@
 import React from "react";
 import "../../assets/css/navbar.css";
 import jQuery from "jquery";
+import Courses from "../Courses/Courses";
 
 class Navbar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      active: "home",
-      isResponsive: true,
-    };
-    this.handleScroll = this.handleScroll.bind(this);
-    this.handleResize = this.handleResize.bind(this);
-  }
-
-  componentDidMount() {
-    window.addEventListener("scroll", this.handleScroll);
-    window.addEventListener("resize", this.handleResize);
-    this.handleResize();
-    if (window.location.pathname === "/home") {
-      this.setState({ active: "home" });
-    } else if (window.location.pathname === "/") {
-      this.setState({ active: "home" });
-    } else if (window.location.pathname === "/about") {
-      this.setState({ active: "about" });
-    } else if (window.location.pathname === "/courses") {
-      this.setState({ active: "courses" });
-    } else if (window.location.pathname === "/contact") {
-      this.setState({ active: "contact" });
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: "home",
+            isResponsive: true,
+        };
+        this.handleScroll = this.handleScroll.bind(this);
+        this.handleResize = this.handleResize.bind(this);
     }
-  }
 
-  componentWillUnmount() {
-    window.removeEventListener("scroll", this.handleScroll);
-    window.removeEventListener("resize", this.handleResize);
-  }
-
-  handleResize(e) {
-    let isResponsive = true;
-
-    if (window.innerWidth > 880) {
-      isResponsive = true;
-    } else {
-      isResponsive = false;
+    componentDidMount() {
+        window.addEventListener("scroll", this.handleScroll);
+        window.addEventListener("resize", this.handleResize);
+        this.handleResize();
+        if (window.location.pathname === "/home") {
+            this.setState({ active: "home" });
+        } else if (window.location.pathname === "/") {
+            this.setState({ active: "home" });
+        } else if (window.location.pathname === "/about") {
+            this.setState({ active: "about" });
+        } else if (window.location.pathname === "/courses") {
+            this.setState({ active: "courses" });
+        } else if (window.location.pathname === "/contact") {
+            this.setState({ active: "contact" });
+        }
     }
-    this.setState({ isResponsive });
-  }
 
-  handleScroll(event) {
-    let scrollTop = window.pageYOffset;
-    if (scrollTop > 10) {
-      jQuery(".navbar-main").css({
-        height: "70px",
-        borderWidth: "0px 0px 4px 0px",
-        backgroundColor: "white",
-        Transition: "1s",
-      });
-    } else {
-      jQuery(".navbar-main").css({
-        height: "80px",
-        width: "100vw",
-        border: "0px",
-        backgroundColor: "transparent",
-        Transition: "1s",
-      });
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScroll);
+        window.removeEventListener("resize", this.handleResize);
     }
-  }
 
-  render() {
-    const { isResponsive } = this.state;
+    handleResize(e) {
+        let isResponsive = true;
 
-    if (isResponsive) {
-      return (
-        <nav className="navbar-main">
-          <div>
-            <h1>THIS IS LOGO</h1>
-          </div>
-          <ul>
-            <li>
-              <a href="/home">Home</a>
-              <div
-                className={this.state.active === "home" ? "circle" : ""}
-              ></div>
-            </li>
-            <li class="dropdown">
-              <a href="#">Services</a>
-              <ul>
-                <li><a href="#">Service 1</a></li>
-                <li><a href="#">Service 2</a></li>
-                <li><a href="#">Service 3</a></li>
-              </ul>
-            </li>
-            <li>
-              <a href="/courses">Courses</a>
-              <div
-                className={this.state.active === "courses" ? "circle" : ""}
-              ></div>
-            </li>
-            <li>
-              <a href="/about">About Us</a>
-              <div
-                className={this.state.active === "about" ? "circle" : ""}
-              ></div>
-            </li>
-            <li>
-              <a href="/contact">Contact</a>
-              <div
-                className={this.state.active === "contact" ? "circle" : ""}
-              ></div>
-            </li>
-          </ul>
-        </nav>
-      );
-    } else {
-      return (
-        <div className="navbar-menuB">
-          <div className="menu-sidebar">
-            <a className="closebtn" onClick={() => closeNav()}>
-              ×
-            </a>
-            <a className="path" href="/Home">
-              Home
-            </a>
-            <a className="path" href="/About">
-              About Us
-            </a>
-            <a className="path" href="/Events">
-              Events
-            </a>
-            <a className="path" href="/Career">
-              Career Counseling
-            </a>
-            <a className="path" href="/Contact">
-              Contact Us
-            </a>
-          </div>
-          <div className="hamburger" onClick={() => openNav()}>
-            ☰
-          </div>
-          {/* <img id="navbar-logo" src={Logo} /> */}
-        </div>
-      );
+        if (window.innerWidth > 880) {
+            isResponsive = true;
+        } else {
+            isResponsive = false;
+        }
+        this.setState({ isResponsive });
     }
+
+    handleScroll(event) {
+        let scrollTop = window.pageYOffset;
+        if (scrollTop > 10) {
+            jQuery(".navbar-main").css({
+                height: "70px",
+                borderWidth: "0px 0px 4px 0px",
+                backgroundColor: "white",
+                Transition: "1s",
+            });
+        } else {
+            jQuery(".navbar-main").css({
+                height: "80px",
+                width: "100vw",
+                border: "0px",
+                backgroundColor: "transparent",
+                Transition: "1s",
+            });
+        }
+    }
+
+    render() {
+        const { isResponsive } = this.state;
+
+        if (isResponsive) {
+            return (
+                <nav className="navbar-main">
+                    <div>
+                        <h1>THIS IS LOGO</h1>
+                    </div>
+                    <ul>
+                        <li>
+                            <a href="/home">Home</a>
+                            <div className={this.state.active === "home" ? "circle" : ""}></div>
+                        </li>
+                        <li className="dropdown">
+                            <a href="#">Services</a>
+                            <ul>
+                                <li>
+                                    <a href="#">ISCE</a>
+                                </li>
+                                <li>
+                                    <a href="#">CBSE</a>
+                                </li>
+                                <li>
+                                    <a href="#">SSC</a>
+                                </li>
+                                <li>
+                                    <a href="#">HSC</a>
+                                </li>
+                                <li>
+                                    <a href="#">JEE</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="/courses">Courses</a>
+                            <div className={this.state.active === "courses" ? "circle" : ""}></div>
+                        </li>
+                        <li>
+                            <a href="/about">About Us</a>
+                            <div className={this.state.active === "about" ? "circle" : ""}></div>
+                        </li>
+                        <li>
+                            <a href="/contact">Contact</a>
+                            <div className={this.state.active === "contact" ? "circle" : ""}></div>
+                        </li>
+                    </ul>
+                </nav>
+            );
+        } else {
+            return (
+                <div className="navbar-menuB">
+                    <div className="menu-sidebar">
+                        <a className="closebtn" onClick={() => closeNav()}>
+                            ×
+                        </a>
+                        <a className="path" href="/Home">
+                            Home
+                        </a>
+                        <a className="path" href="/About">
+                            About Us
+                        </a>
+                        <a className="path" href="/Events">
+                            Events
+                        </a>
+                        <a className="path" href="/Career">
+                            Career Counseling
+                        </a>
+                        <a className="path" href="/Contact">
+                            Contact Us
+                        </a>
+                    </div>
+                    <div className="hamburger" onClick={() => openNav()}>
+                        ☰
+                    </div>
+                    {/* <img id="navbar-logo" src={Logo} /> */}
+                </div>
+            );
+        }
         function openNav() {
             jQuery(".menu-sidebar").css({
                 width: "300px",
