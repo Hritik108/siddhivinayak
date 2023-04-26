bbe4fd0bc68342563d9ee66162549e084c688fbb
Prashant33-py <pvasala786@gmail.com>
2023-04-26 19:50:55 +0530

    minor changes in contact page

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,61 +1,60 @@
 import React, { Component } from "react";
 import "../../assets/css/contact.css";
-import { IoCall, IoLocationSharp } from "react-icons/io5";
-import { Container } from "@mui/material";
+import contactBackgroundImg from "./contactbgimg.jpg";
 
 class Contact extends Component {
     render() {
         return (
-            <div>
-                <div className="contact-headings">
-                    <h1>Get in touch</h1>
-                    <p>
-                        Want to get in touch? We'd love to hear from you.
-                        <br /> Here's how you can reach us...
-                    </p>
+            <div id="contact">
+                <div id="contactHeader">
+                    <div className="w-75 mx-auto py-5">
+                        <h1>Get in Touch</h1>
+                        <p>
+                            Want to get in touch? We'd love to hear from you.
+                            <br /> Here's how you can reach us...
+                        </p>
+                    </div>
                 </div>
-                <div className="contact">
-                    <div className="contact-body">
-                        <div className="cards">
-                            <div className="card">
-                                <Container>
-                                    <div className="card-icon">
-                                        <IoCall />
-                                    </div>
-                                    <h3 className="card-title">Talk to Us</h3>
-                                    <p className="card-text">
-                                        Interested to join? You can feel free to call us anytime <br />
-                                        during our working hours on the following contact numbers
-                                    </p>
-                                    <ul>
-                                        <li>
-                                            <a href="tel:9819285180">+91 98192 85180</a>
-                                        </li>
-                                        <li>
-                                            <a href="tel:8082772648">+91 80827 72648</a>
-                                        </li>
-                                    </ul>
-                                </Container>
+                <div class="row w-75 mx-auto">
+                    <div class="col-md-6 col-sm-12 col-xs-12 mb-3">
+                        <div class="card h-100 text-center">
+                            <div class="card-body">
+                                <h2 class="card-title">Talk to Us</h2>
+                                <p class="card-text px-5">
+                                    Interested to join? You can feel free to
+                                    call us anytime during our working hours on
+                                    the following contact numbers
+                                </p>
+                                <ul>
+                                    <li>
+                                        <a href="tel:9819285180">
+                                            +91 98192 85180
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="tel:8082772648">
+                                            +91 80827 72648
+                                        </a>
+                                    </li>
+                                </ul>
                             </div>
-
-                            <div className="card">
-                                <Container>
-                                    <div className="card-icon">
-                                        <IoLocationSharp />
-                                    </div>
-                                    <h3 className="card-title">Find Us Here</h3>
-                                    <p className="card-text">
-                                        Shop No 6, Siddhivinayak Classes, Near Agar Bazar Bus Stop,
-                                        <br />
-                                        Agar Bazaar, Dadar West, Mumbai - 400028
-                                    </p>
-                                    <br />
-                                    <p className="card-text">
-                                        At Ratan, Shop No: 10, S.K. Bole Road, Agar Bazaar, Near
-                                        <br />
-                                        Siddhivinayak Temple, Dadar West, Mumbai - 400028
-                                    </p>
-                                </Container>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12 col-xs-12 mb-3">
+                        <div class="card h-100 text-center">
+                            <div class="card-body">
+                                <h2 class="card-title">Find Us Here</h2>
+                                <p className="card-text px-5">
+                                    Shop No 6, Siddhivinayak Classes, Near Agar
+                                    Bazar Bus Stop, Agar Bazaar, Dadar West,
+                                    Mumbai - 400028
+                                </p>
+                                <br />
+                                <p className="card-text px-5">
+                                    At Ratan, Shop No: 10, S.K. Bole Road, Agar
+                                    Bazaar, Near Siddhivinayak Temple, Dadar
+                                    West, Mumbai - 400028
+                                </p>
                             </div>
                         </div>
                     </div>
