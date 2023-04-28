3691e75aecffe82f6bfed6dfef40d1c39dd2f621
Prashant33-py <pvasala786@gmail.com>
2023-04-28 11:52:16 +0530

    contact page updated

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,62 +1,43 @@
 import React, { Component } from "react";
 import "../../assets/css/contact.css";
-import contactBackgroundImg from "./contactbgimg.jpg";
+import { FaHome, FaPhoneAlt } from "react-icons/fa";
 
 class Contact extends Component {
     render() {
         return (
-            <div id="contact">
-                <div id="contactHeader">
-                    <div className="w-75 mx-auto py-5">
-                        <h1>Get in Touch</h1>
-                        <p>
-                            Want to get in touch? We'd love to hear from you.
-                            <br /> Here's how you can reach us...
+            <div id="contactHeader">
+                <div className="w-75 mx-auto py-5">
+                    <h1 className="fs-1">Get in Touch</h1>
+                    <p>
+                        Want to get in touch? We'd love to hear from you. Here's
+                        how you can reach us...
+                    </p>
+                    <div className="p-0">
+                        <div className="d-flex">
+                            <FaPhoneAlt className="mt-1 me-2" />
+                            <h4>Phone</h4>
+                        </div>
+                        <p className="my-1">
+                            <a href="tel:9819285180">+91 98192 85180</a>
+                        </p>
+                        <p className="me-1">
+                            <a href="tel:8082772648">+91 80827 72648</a>
                         </p>
                     </div>
-                </div>
-                <div class="row w-75 mx-auto">
-                    <div class="col-md-6 col-sm-12 col-xs-12 mb-3">
-                        <div class="card h-100 text-center">
-                            <div class="card-body">
-                                <h2 class="card-title">Talk to Us</h2>
-                                <p class="card-text px-5">
-                                    Interested to join? You can feel free to
-                                    call us anytime during our working hours on
-                                    the following contact numbers
-                                </p>
-                                <ul>
-                                    <li>
-                                        <a href="tel:9819285180">
-                                            +91 98192 85180
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="tel:8082772648">
-                                            +91 80827 72648
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
+                    <div>
+                        <div className="d-flex">
+                            <FaHome className="mt-1 me-2" />
+                            <h4>Address</h4>
                         </div>
-                    </div>
-                    <div class="col-md-6 col-sm-12 col-xs-12 mb-3">
-                        <div class="card h-100 text-center">
-                            <div class="card-body">
-                                <h2 class="card-title">Find Us Here</h2>
-                                <p className="card-text px-5">
-                                    Shop No 6, Siddhivinayak Classes, Near Agar
-                                    Bazar Bus Stop, Agar Bazaar, Dadar West,
-                                    Mumbai - 400028
-                                </p>
-                                <br />
-                                <p className="card-text px-5">
-                                    At Ratan, Shop No: 10, S.K. Bole Road, Agar
-                                    Bazaar, Near Siddhivinayak Temple, Dadar
-                                    West, Mumbai - 400028
-                                </p>
-                            </div>
-                        </div>
+                        <p className="my-1">
+                            - Shop No 6, Siddhivinayak Classes, Near Agar Bazar
+                            Bus Stop, Agar Bazaar, Dadar West, Mumbai - 400028
+                        </p>
+                        <p className="my-1">
+                            - At Ratan, Shop No: 10, S.K. Bole Road, Agar
+                            Bazaar, Near Siddhivinayak Temple, Dadar West,
+                            Mumbai - 400028
+                        </p>
                     </div>
                 </div>
             </div>
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,157 +1,174 @@
 import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
-import React, { Component } from "react";
+import React from "react";
 import classesLogo from "../../assets/images/classesLogo.png";
-import "../../assets/css/_footer.css"
+import "../../assets/css/_footer.css";
 
 export default function Footer() {
-	return (
-		<footer className="footer-wrapper">
-			<div className="footer-widgets-wrapper">
-				<div className="container">
-					<div className="row">
-						<div className="col-xl-3 col-lg-4 col-md-6 col-12">
-							<div className="single-footer-widget wow fadeInLeft">
-								<div className="about-us-widget">
-									<a href="/">
-										<a className="footer-logo d-block">
-                                        <img src={classesLogo} alt="Classes Logo" width={100} />
-										</a>
-									</a>
-									<p>
-										Xmoze helps millions of people get the best Wordpress theme design and offers
-										software reviews, ratings, comprehensive services.
-									</p>
-								</div>
-							</div>
-						</div>
-						<div className="col-xl-2 col-lg-3 offset-xl-1 col-md-6 col-12">
-							<div className="single-footer-widget wow fadeInLeft" data-wow-delay=".2s">
-								<div className="widget-title">
-									<h5>Company</h5>
-								</div>
-								<ul>
-									<li>
-										<a href="/about">
-											<a>About xmoze</a>
-										</a>
-									</li>
-									<li>
-										<a href="/contact">
-											<a>Contact & support</a>
-										</a>
-									</li>
-									<li>
-										<a href="/#">
-											<a>Setting & privacy</a>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<a>Setting & privacy</a>
-										</a>
-									</li>
-								</ul>
-							</div>
-						</div>
-						<div className="col-xl-2 col-lg-3 offset-xl-1 col-md-6 col-12">
-							<div className="single-footer-widget wow fadeInLeft" data-wow-delay=".4s">
-								<div className="widget-title">
-									<h5>Services</h5>
-								</div>
-								<ul>
-									<li>
-										<a href="#">
-											<a>Incident responder</a>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<a>Secure managed IT</a>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<a>Check website Url</a>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<a>Locker security</a>
-										</a>
-									</li>
-								</ul>
-							</div>
-						</div>
-						<div className="col-xl-2 col-lg-3 offset-xl-1 col-md-6 col-12">
-							<div className="single-footer-widget wow fadeInLeft" data-wow-delay=".6s">
-								<div className="widget-title">
-									<h5>Resources</h5>
-								</div>
-								<ul>
-									<li>
-										<a href="#">
-											<a>Payment plans</a>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<a>Blogs & guides</a>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<a>Premium support</a>
-										</a>
-									</li>
-									<li>
-										<a href="#">
-											<a>Our products</a>
-										</a>
-									</li>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className="footer-bottom-wrapper">
-				<div className="container">
-					<div className="footer-bottom-content d-md-flex justify-content-between">
-						<div className="site-copyright wow fadeInUp" data-wow-delay=".2" data-wow-duration="1s">
-							<p>
-								&copy; 2022{" "}
-								<a href="#">
-									<a>Xmooze Theme</a>
-								</a>{" "}
-								All Rights Reserved.
-							</p>
-						</div>
-						<div
-							className="social-as mt-4 mt-md-0 wow fadeInUp"
-							data-wow-delay=".3"
-							data-wow-duration="1s"
-						>
-							<a href="#">
-								<a>
-									<FaFacebookF />
-								</a>
-							</a>
-							<a href="#">
-								<a>
-									<FaTwitter />
-								</a>
-							</a>
-							<a href="#">
-								<a>
-									<FaInstagram />
-								</a>
-							</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</footer>
-	);
+    return (
+        <footer className="footer-wrapper">
+            <div className="footer-widgets-wrapper">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xl-3 col-lg-4 col-md-6 col-12">
+                            <div className="single-footer-widget wow fadeInLeft">
+                                <div className="about-us-widget">
+                                    <a href="/">
+                                        <a className="footer-logo d-block">
+                                            <img
+                                                src={classesLogo}
+                                                alt="Classes Logo"
+                                                width={100}
+                                            />
+                                        </a>
+                                    </a>
+                                    <p>
+                                        Xmoze helps millions of people get the
+                                        best Wordpress theme design and offers
+                                        software reviews, ratings, comprehensive
+                                        services.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-xl-2 col-lg-3 offset-xl-1 col-md-6 col-12">
+                            <div
+                                className="single-footer-widget wow fadeInLeft"
+                                data-wow-delay=".2s"
+                            >
+                                <div className="widget-title">
+                                    <h5>Company</h5>
+                                </div>
+                                <ul>
+                                    <li>
+                                        <a href="/about">
+                                            <a>About xmoze</a>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="/contact">
+                                            <a>Contact & support</a>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="/#">
+                                            <a>Setting & privacy</a>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <a>Setting & privacy</a>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-xl-2 col-lg-3 offset-xl-1 col-md-6 col-12">
+                            <div
+                                className="single-footer-widget wow fadeInLeft"
+                                data-wow-delay=".4s"
+                            >
+                                <div className="widget-title">
+                                    <h5>Services</h5>
+                                </div>
+                                <ul>
+                                    <li>
+                                        <a href="#">
+                                            <a>Incident responder</a>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <a>Secure managed IT</a>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <a>Check website Url</a>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <a>Locker security</a>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-xl-2 col-lg-3 offset-xl-1 col-md-6 col-12">
+                            <div
+                                className="single-footer-widget wow fadeInLeft"
+                                data-wow-delay=".6s"
+                            >
+                                <div className="widget-title">
+                                    <h5>Resources</h5>
+                                </div>
+                                <ul>
+                                    <li>
+                                        <a href="#">
+                                            <a>Payment plans</a>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <a>Blogs & guides</a>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <a>Premium support</a>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <a>Our products</a>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="footer-bottom-wrapper">
+                <div className="container">
+                    <div className="footer-bottom-content d-md-flex justify-content-between">
+                        <div
+                            className="site-copyright wow fadeInUp"
+                            data-wow-delay=".2"
+                            data-wow-duration="1s"
+                        >
+                            <p>
+                                &copy; 2022{" "}
+                                <a href="#">
+                                    <a>Xmooze Theme</a>
+                                </a>{" "}
+                                All Rights Reserved.
+                            </p>
+                        </div>
+                        <div
+                            className="social-as mt-4 mt-md-0 wow fadeInUp"
+                            data-wow-delay=".3"
+                            data-wow-duration="1s"
+                        >
+                            <a href="#">
+                                <a>
+                                    <FaFacebookF />
+                                </a>
+                            </a>
+                            <a href="#">
+                                <a>
+                                    <FaTwitter />
+                                </a>
+                            </a>
+                            <a href="#">
+                                <a>
+                                    <FaInstagram />
+                                </a>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
 }
-
-
